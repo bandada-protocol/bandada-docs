c82799201bd4f1d9e4a1bd192cd61f4e993460f2
vplasencia <v.pcalana@gmail.com>
2024-02-04 22:01:46 +0100

    refactor: order articles and videos starting with the most recent

--- src/components/RenderArticles/index.tsx
+++ src/components/RenderArticles/index.tsx
@@ -3,19 +3,24 @@ import articles from "../../../static/data/articles.json"
 export default function RenderArticles() {
   return (
     <div>
-      {articles.map((article) => (
-        <div style={{margin: "15px"}}>
-          {" "}
-          <a
-            href={article.url}
-            target="_blank"
-            rel="noreferrer noopener nofollow"
-          >
-            {article.title}
-          </a>{" "}
-          - {article.authors.join(", ")}{" "}
-        </div>
-      ))}
+      {articles
+        .sort(
+          (a, b) =>
+            new Date(b.date || "").getTime() - new Date(a.date || "").getTime()
+        )
+        .map((article) => (
+          <div style={{ margin: "15px" }}>
+            {" "}
+            <a
+              href={article.url}
+              target="_blank"
+              rel="noreferrer noopener nofollow"
+            >
+              {article.title}
+            </a>{" "}
+            - {article.authors.join(", ")}{" "}
+          </div>
+        ))}
     </div>
   )
 }
--- src/components/RenderVideos/index.tsx
+++ src/components/RenderVideos/index.tsx
@@ -3,19 +3,24 @@ import videos from "../../../static/data/videos.json"
 export default function RenderVideos() {
   return (
     <div>
-      {videos.map((video) => (
-        <div style={{margin: "15px"}}>
-          {" "}
-          <a
-            href={video.url}
-            target="_blank"
-            rel="noreferrer noopener nofollow"
-          >
-            {video.title}
-          </a>{" "}
-          - {video.speakers.join(", ")}{" "}
-        </div>
-      ))}
+      {videos
+        .sort(
+          (a, b) =>
+            new Date(b.date || "").getTime() - new Date(a.date || "").getTime()
+        )
+        .map((video) => (
+          <div style={{ margin: "15px" }}>
+            {" "}
+            <a
+              href={video.url}
+              target="_blank"
+              rel="noreferrer noopener nofollow"
+            >
+              {video.title}
+            </a>{" "}
+            - {video.speakers.join(", ")}{" "}
+          </div>
+        ))}
     </div>
   )
 }
