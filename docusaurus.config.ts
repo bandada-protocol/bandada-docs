6190a07611264f9cdf112bed648669937ce90418
vplasencia <v.pcalana@gmail.com>
2024-02-04 12:51:12 +0100

    refactor: update logo and favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "@docusaurus/types"
 import type * as Preset from "@docusaurus/preset-classic"
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Bandada docs",
+  tagline: "Bandada documentation",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -58,9 +58,9 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
+      title: "",
       logo: {
-        alt: "My Site Logo",
+        alt: "Bandada Logo",
         src: "img/logo.svg"
       },
       items: [
@@ -121,7 +121,7 @@ const config: Config = {
           ]
         }
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+      copyright: `Copyright © ${new Date().getFullYear()} Ethereum Foundation`
     },
     prism: {
       theme: prismThemes.github,
